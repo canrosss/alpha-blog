cc337bee46005da5d9eafcc0f8dd5b7e999e10b8
Canros <canros.mx@gmail.com>
2017-10-12 02:58:29 -0500

    Styling errors  messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
     if @article.save
       #do something
-      flash[:notice] = "Article was a successfully created"
+      flash[:success] = "Article was a successfully created"
       redirect_to articles_path(@article)
     else
       render 'new'
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
 
   def update(*args)
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to articles_path
     else
       render 'edit'
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
   def destroy(*args)
     #Buscamos el articulo basado en el id
     @article.destroy
-    flash[:notice] = "Article was successfully deleted!"
+    flash[:danger] = "Article was successfully deleted!"
     redirect_to articles_path
   end
 

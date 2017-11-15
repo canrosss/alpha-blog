5a1206182fcfe19fd17dad5dbeccbf269029f063
Canros <canros.mx@gmail.com>
2017-11-15 04:49:17 -0600

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :tittle, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
   validates :user_id, presence: true
